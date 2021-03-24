ac5f4a1a73b008dd34cc9220c32e3861e9395137
morozoleh <olegmoroz93@gmail.com>
2021-03-24 16:03:34 +0200

    Updates

---
+++ precache-manifest.77928a864d5e28fd128ea87a4d5ce803.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd44da0a14b9bf973b4af1a1133289d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "50fd606239c2cd2a8acd",
+    "url": "./static/css/2.d1ab60d4.chunk.css"
+  },
+  {
+    "revision": "d88d3b8f498af1f3959c",
+    "url": "./static/css/main.3d353fc0.chunk.css"
+  },
+  {
+    "revision": "50fd606239c2cd2a8acd",
+    "url": "./static/js/2.94425468.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.94425468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88d3b8f498af1f3959c",
+    "url": "./static/js/main.87ab7f87.chunk.js"
+  },
+  {
+    "revision": "12bee12ab4b288faf1d5",
+    "url": "./static/js/runtime-main.108729f7.js"
+  }
+]);
